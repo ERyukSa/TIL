9c26ba94b9a4cdaf97e3d5d7d4b4ad8294adfd35
SH_H264 <48471292+jslkap227@users.noreply.github.com>
2021-07-09 00:42:53 +0900

    2021-07-08 TIL: BMI 계산기

---
+++ 안드로이드/패스트캠퍼스/README.md
@@ -0,0 +1,15 @@
+# [30개 프로젝트로 배우는 Android 앱 개발 with Kotlin 초격차 패키지 Online.](https://fastcampus.co.kr/dev_online_androidappfinal)
+
+Kotlin과 함께 30가지 프로젝트로 안드로이드 앱 개발을 배우는 패스트캠퍼스의 온라인 강의
+
+---
+
+1. Basic
+   - BMI 계산기
+
+<br>
+
+1. Intermediate
+2. Upper Intermediate
+3. Advanced
+4. Final
---
+++ 안드로이드/패스트캠퍼스/프로젝트1_BMI_계산기/project1_BMI/app/src/main/java/part2/ch01/BMI/MainActivity.kt
@@ -0,0 +1,36 @@
+package part2.ch01.BMI
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
+        resultButton.setOnClickListener {
+
+            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // 이 아래로는 절대 빈 값이 올 수 없다.
+            
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+            startActivity(intent)
+        }
+    }
+}
